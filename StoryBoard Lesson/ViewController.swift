427bcc345f9853f2093d5e5feca1f0a5096a74db
Leonardo Gómez <gmezleo97@gmail.com>
2020-04-14 12:23:22 -0500

    Initial Commit

---
+++ StoryBoard Lesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoard Lesson
+//
+//  Created by Leonardo Gómez on 4/14/20.
+//  Copyright © 2020 Leonardo Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
